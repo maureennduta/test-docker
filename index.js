2e80ee723cc1018035c61df55485fcdfc437e274
Maureen Nduta <67265839+maureennduta@users.noreply.github.com>
2021-07-19 09:41:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,12 @@
 function start(){
   console.log('App is running ...');
 }
+function test(){
+  return false;
+}
+setInterval(function(){
+  console.log('print after every 10 seconds ')
+},10000);
 
 start();
+test();
